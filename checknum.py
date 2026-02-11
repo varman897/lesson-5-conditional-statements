6f9cb45aa73f603542a758f20ab9a02456905ab1
varman897 <varmansivarooban2015@gmail.com>
2026-02-10 20:04:59 -0800

    checknum

---
+++ checknum.py
@@ -0,0 +1,10 @@
+i=int(input("Enter a number: "))
+if(i<15):
+    print("i is smaller than 15")
+    print("I'm an if block")
+
+else:
+    
+    print("i is greater than 15")
+    print("I'm an else block")
+    print("i is not a if or else block")
